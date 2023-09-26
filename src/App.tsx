a6260536ff2999dfc8930954b6d4640c723b6e72
Sergio Hernandez <ehernandez.sergio@gmail.com>
2023-09-25 22:37:21 -0700

    Text draggable, font size and family editable

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,78 @@ import ColorPicker from "./components/ColorPicker";
 import "./App.css";
 
 function App() {
-  const [width, setWidth] = useState(300);
-  const [height, setHeight] = useState(500);
-  const [selectedColor, setSelectedColor] = useState("#ff9900"); // Default color is white
-  const [text, setText] = useState<string>("Jade and West Wedding"); // Default text
+  const [width, setWidth] = useState(546);
+  const [height, setHeight] = useState(1600);
+  const [selectedColor, setSelectedColor] = useState("#ff9900");
+  const [text, setText] = useState<string>("Jade and West Wedding");
+  const [fontSize, setFontSize] = useState<number>(44); // Default font size
+  const [fontFamily, setFontFamily] = useState<string>("Playfair Display"); // Default font family
+  const [textYPosition, setTextYPosition] = useState<number>(930);
+
+  // Function to update the variable
+  const updatetextYPosition = (updatedValue: number) => {
+    setTextYPosition(updatedValue);
+  };
+
+  const fontSizes = ["32px", "38px", "44px", "60px"];
+  const fontFamilies = [
+    "Caveat",
+    "Croissant One",
+    "Dancing Script",
+    "Inter",
+    "Lato",
+    "Playfair Display",
+  ];
+
+  function getLines(
+    ctx: CanvasRenderingContext2D,
+    text: string,
+    maxWidth: number
+  ) {
+    var words = text.split(" ");
+    var lines = [];
+    var currentLine = words[0];
+
+    for (var i = 1; i < words.length; i++) {
+      var word = words[i];
+      var thisWidth = ctx.measureText(currentLine + " " + word).width;
+      if (thisWidth < maxWidth) {
+        currentLine += " " + word;
+      } else {
+        lines.push(currentLine);
+        currentLine = word;
+      }
+    }
+    lines.push(currentLine);
+    return lines;
+  }
+
+  function downloadImage() {
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas.getContext("2d");
+    if (ctx) {
+      ctx.fillStyle = selectedColor;
+      ctx.fillRect(0, 0, width, height);
+
+      ctx.fillStyle = "#ffffff";
+      ctx.font = `${fontSize}px ${fontFamily}`;
+      ctx.textAlign = "center";
+      const textLines = getLines(ctx, text, width);
+      for (let i = 0; i < textLines.length; i++) {
+        const line = textLines[i];
+        const lineHeight = 1.5 * fontSize;
+        ctx.fillText(line, width / 2, textYPosition + 930 + i * lineHeight);
+      }
+
+      const url: string = canvas.toDataURL("image/png");
+      const link: HTMLAnchorElement = document.createElement("a");
+      link.href = url;
+      link.download = "custom-image.png";
+      link.click();
+    }
+  }
 
   const handleColorChange = (newColor: string) => {
     setSelectedColor(newColor);
@@ -17,10 +85,31 @@ function App() {
     setText(event.target.value);
   };
 
+  const handleFontSizeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setFontSize(parseInt(event.target.value));
+  };
+
+  const handleFontFamilyChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setFontFamily(event.target.value);
+  };
+
   return (
     <div className="app">
       <h1>Photobooth Layout Maker</h1>
-      <Image color={selectedColor} width={width} height={height} text={text} />
+      <Image
+        color={selectedColor}
+        width={width / 2.5}
+        height={height / 2.5}
+        text={text}
+        fontSize={fontSize / 2}
+        fontFamily={fontFamily}
+        textYPosition={textYPosition}
+        updatetextYPosition={updatetextYPosition}
+      />
       <ColorPicker onChange={handleColorChange} color={selectedColor} />
       <input
         type="text"
@@ -28,35 +117,37 @@ function App() {
         onChange={handleTextChange}
         placeholder="Enter text here"
       />
-      <button onClick={() => downloadImage(selectedColor, text)}>
-        Download Image
-      </button>
+      <div>
+        <label htmlFor="fontSize">Font Size:</label>
+        <select
+          id="fontSize"
+          onChange={handleFontSizeChange}
+          value={`${fontSize}px`}
+        >
+          {fontSizes.map((size) => (
+            <option key={size} value={size}>
+              {size}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="fontFamily">Font Family:</label>
+        <select
+          id="fontFamily"
+          onChange={handleFontFamilyChange}
+          value={fontFamily}
+        >
+          {fontFamilies.map((family) => (
+            <option key={family} value={family}>
+              {family}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button onClick={() => downloadImage()}>Download Image</button>
     </div>
   );
-}
-
-function downloadImage(color: string, text: string) {
-  const imageWidth = 300;
-  const imageHeight = 500;
-  const canvas = document.createElement("canvas");
-  canvas.width = imageWidth;
-  canvas.height = imageHeight;
-  const ctx = canvas.getContext("2d");
-  if (ctx) {
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, imageWidth, imageHeight);
-
-    ctx.fillStyle = "#ffffff"; // Set the text color to white
-    ctx.font = "24px Arial";
-    ctx.textAlign = "center";
-    ctx.fillText(text, imageWidth / 2, imageHeight - 20);
-
-    const url: string = canvas.toDataURL("image/png");
-    const link: HTMLAnchorElement = document.createElement("a");
-    link.href = url;
-    link.download = "custom-image.png";
-    link.click();
-  }
 }
 
 export default App;
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./image.css";
 
 interface ImageProps {
@@ -6,9 +6,67 @@ interface ImageProps {
   width: number;
   height: number;
   text: string;
+  fontSize: number;
+  fontFamily: string;
+  textYPosition: number;
+  updatetextYPosition: (updatedValue: number) => void;
 }
 
-const Image: React.FC<ImageProps> = ({ color, width, height, text }) => {
+const Image: React.FC<ImageProps> = ({
+  color,
+  width,
+  height,
+  text,
+  fontSize,
+  fontFamily,
+  updatetextYPosition,
+}) => {
+  const [position, setPosition] = useState({ x: 0, y: 550 });
+  const [dragging, setDragging] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+  const textRef = useRef<HTMLSpanElement | null>(null);
+
+  useEffect(() => {
+    if (textRef.current) {
+      const handleMouseDown = (e: MouseEvent) => {
+        setDragging(true);
+        const boundingBox = textRef.current!.getBoundingClientRect();
+        const offsetY = e.clientY - boundingBox.top + 90;
+        setOffset({ x: 0, y: offsetY }); // Fixed X position to 0
+      };
+
+      const handleMouseMove = (e: MouseEvent) => {
+        if (dragging) {
+          const newY = e.clientY - offset.y;
+
+          // Ensure the text stays within the canvas boundaries
+          const maxY = height - textRef.current!.offsetHeight;
+          const minY = height - 150; // Minimum Y position 200px from the bottom
+
+          // Restrict the Y position
+          const clampedY = Math.max(minY, Math.min(maxY, newY));
+
+          setPosition({ x: 0, y: clampedY });
+          updatetextYPosition(clampedY);
+        }
+      };
+
+      const handleMouseUp = () => {
+        setDragging(false);
+      };
+
+      textRef.current.addEventListener("mousedown", handleMouseDown);
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp);
+
+      return () => {
+        textRef.current!.removeEventListener("mousedown", handleMouseDown);
+        document.removeEventListener("mousemove", handleMouseMove);
+        document.removeEventListener("mouseup", handleMouseUp);
+      };
+    }
+  }, [dragging, offset, height]);
+
   return (
     <div className="customizable-image-container">
       <div
@@ -17,13 +75,24 @@ const Image: React.FC<ImageProps> = ({ color, width, height, text }) => {
           backgroundColor: color,
           width: `${width}px`,
           height: `${height}px`,
+          position: "relative",
+          textAlign: "center", // Center text horizontally
         }}
       >
         <span
           style={{
-            color: "white", // Set the text color (you can adjust this as needed)
-            fontSize: "24px", // Set the text font size (you can adjust this as needed)
+            color: "white",
+            fontSize: `${fontSize}px`,
+            fontFamily: fontFamily,
+            userSelect: "none",
+            cursor: dragging ? "grabbing" : "grab",
+            position: "absolute",
+            left: `${position.x}px`,
+            top: `${position.y}px`,
+            width: "100%", // Expand the text to 100% width for centering
           }}
+          draggable="false"
+          ref={textRef}
         >
           {text}
         </span>
